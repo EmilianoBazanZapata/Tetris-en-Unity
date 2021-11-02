a471284cbae44c2c4f5f195b1424887995bf3403
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-11-02 11:53:04 -0300

    El spawner de piezas aleatorias

---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] LevelPices;
+    private void Start() {
+        SpawnNetPiece();
+    }
+    public void SpawnNetPiece()
+    {
+        //decido aleatoriamente que pieza debe spawnearse
+        int i = Random.Range(0,LevelPices.Length);
+        //se instancia en la misma posicion del padre ademas de mantener su rotacion
+        Instantiate(LevelPices[i],this.transform.position, Quaternion.identity);
+    }
+}
